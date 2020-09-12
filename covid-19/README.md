c06185f348eb75ac99bc3dfb8b5365b25d5e94e1
Pedro Portella <pedro.kadjin.sg@gmail.com>
2020-09-12 18:24:05 -0300

    covid-19

---
+++ covid-19/README.md
@@ -0,0 +1,4 @@
+# covid-19 :low_brightness: :pill:
+<img src="https://github.com/PedroPadilhaPortella/covid-19/blob/master/covid.PNG" style="text-align=center" alt="covid19">
+
+### Aplicação Web para listar dados a respeito do covid-19 criado durante o projeto da digital innovation one, usando React e o modelo PWA
---
+++ covid-19/src/commons/constants/countries.js
@@ -0,0 +1,169 @@
+import BrazilFlag from '../../assets/images/brazil.png'
+
+const COUNTRIES = [
+    {
+    label: 'Mundo',
+    value: 'world',
+  },
+  {
+    label: 'Alemanha',
+    value: 'germany',
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+    flag: BrazilFlag
+  },
+  {
+    label: 'China',
+    value: 'china',
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+  },
+  {
+    label: 'Portugal',
+    value: 'portugal',
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+  },
+  {
+    label: 'França',
+    value: 'france',
+  },
+  {
+    label: 'Reino Unido',
+    value: 'uk',
+  },
+  {
+    label: 'Vietnã',
+    value: 'Vietnam',
+  },
+  {
+    label: 'Uruguai',
+    value: 'uruguay',
+  },
+  {
+    label: 'Nova Zelândia',
+    value: 'New Zealand',
+  },
+  {
+    label: 'Grécia',
+    value: 'Greece',
+  },
+  {
+    label: 'Síria',
+    value: 'syria',
+  },
+  {
+    label: 'Cuba',
+    value: 'Cuba',
+  },
+  {
+    label: 'Sri Lanka',
+    value: 'Sri Lanka',
+  },
+  {
+    label: 'Ruanda',
+    value: 'Rwanda',
+  },
+  {
+    label: 'Finlândia',
+    value: 'finland',
+  },
+  {
+    label: 'Paraguai',
+    value: 'paraguay',
+  },
+  {
+    label: 'Líbano',
+    value: 'lebanon',
+  },
+  {
+    label: 'Noruega',
+    value: 'norway',
+  },
+  {
+    label: 'Bulgária',
+    value: 'bulgary',
+  },
+  {
+    label: 'Austrália',
+    value: 'Australia',
+  },
+  {
+    label: 'Áustria',
+    value: 'Austria',
+  },
+  {
+    label: 'Venezuela',
+    value: 'Venezuela',
+  },
+  {
+    label: 'Afeganistão',
+    value: 'Afghanistan',
+  },
+  {
+    label: 'Suíça',
+    value: 'Switzerland',
+  },
+  {
+    label: 'Cingapura',
+    value: 'Singapore',
+  },
+  {
+    label: 'Polônia',
+    value: 'Poland',
+  },
+  {
+    label: 'Holanda',
+    value: 'Netherlands',
+  },
+    {
+    label: 'Israel',
+    value: 'Israel',
+  },
+    {
+    label: 'Bélgica',
+    value: 'Belgium',
+  },
+  {
+    label: 'Casaquistão',
+    value: 'Kazakhstan',
+  },
+  {
+    label: 'Arábia Saudita',
+    value: 'Saudi Arabia',
+  },
+  {
+    label: 'Bélgica',
+    value: 'Belgium',
+  },
+      {
+    label: 'Bolívia',
+    value: 'Bolivia',
+  },
+  {
+    label: 'Rússia',
+    value: 'russia',
+  },
+  {
+    label: 'India',
+    value: 'India',
+  },
+  {
+    label: 'Chile',
+    value: 'Chile',
+  },
+
+
+]
+
+export default COUNTRIES
