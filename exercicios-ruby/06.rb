8da12c4d4dc4b5d5f5bf86ea52e4249cfa10d163
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2020-11-20 22:16:46 -0300

    ruby

---
+++ exercicios-ruby/06.rb
@@ -0,0 +1,19 @@
+a = 4
+b = 3
+
+if a > b
+    puts "a > b"
+elsif a == b
+    puts "a == b"
+else
+    puts "b > a"
+end
+
+if a > b then puts "a maior que b" else puts "b maior ou igual que a" end
+
+x = 10
+
+puts "10 é divisivel por 2" if 10 % 2 == 0
+
+puts "a > b" unless a < b
+puts "a > b" if a > b
---
+++ exercicios-ruby/07.rb
@@ -0,0 +1,13 @@
+# Case
+marca = "sla"
+
+case marca
+    when "tesla"
+        puts marca
+    when "ford"
+        puts marca
+    when "ferrari"
+        puts marca
+    else
+        puts "marca padrão"
+end
