54282c5bad266f832fc997d655c7c54be141967b
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-02-04 23:59:23 -0300

    comparators e comparable

---
+++ Cursos Java/Collections e Streams/Comparators.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+import entities.*;
+
+public class Comparators {
+    public static void main(String[] args) {
+        
+        /*
+          Interface usada para ordenação de objetos complexos e classes 
+        criadas pelo desenvolvedor a partir de tipos primitivos.
+          Usado principalment em List<Classe>;
+        */
+
+        List<Estudante> estudantes = new ArrayList<>();
+
+        estudantes.add(new Estudante("Pedro", 19));
+        estudantes.add(new Estudante("Casalli", 20));
+        estudantes.add(new Estudante("Bunhak", 18));
+        estudantes.add(new Estudante("Vini", 19));
+        estudantes.add(new Estudante("Agnes", 18));
+
+        System.out.println("Ordem de insercao" + estudantes);
+
+        estudantes.sort((first, second) -> first.getIdade() - second.getIdade());
+        System.out.println("ordem natural pela idade: " + estudantes);
+
+        estudantes.sort((first, second) -> second.getIdade() - first.getIdade());
+        System.out.println("ordem inversa pela idade: " + estudantes);
+        
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade));
+        System.out.println("ordem natural com Comparator: " + estudantes);
+
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade).reversed());
+        System.out.println("ordem reversa com Comparator: " + estudantes);
+
+        estudantes.sort(Comparator.comparingInt(Estudante::getIdade).reversed());
+        System.out.println("ordem reversa com Comparator: " + estudantes);
+
+        Collections.sort(estudantes);
+        System.out.println("ordem natural usando Collections.sort: " + estudantes);
+        
+        Collections.reverse(estudantes);
+        System.out.println("ordem natural usando Collections.sort: " + estudantes);
+
+        Collections.sort(estudantes, new EstudanteOrdemReversa());
+        System.out.println("ordem natural usando Collections.sort e new EstudanteOrdemReversa(): " + estudantes);
+    }
+}
