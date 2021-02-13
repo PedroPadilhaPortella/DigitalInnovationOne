d7b039cd2ab7465236a0fe1e81d221650e9ae69e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-02-12 23:21:29 -0300

    angular com ngrx

--- Cursos Angular/Aplicacoes Avancadas com Angular/jv-state-ngrx-master/src/app/state/app.actions.ts
+++ Cursos Angular/Aplicacoes Avancadas com Angular/jv-state-ngrx-master/src/app/state/app.actions.ts
@@ -2,15 +2,15 @@ import { createAction, props } from '@ngrx/store';
 import { User } from '../shared/models/user.model';
 
 export const doLogin = createAction(
-  '[Login] Do Login',
-  props<{ name: string, email: string }>(),
+    '[Login] Do Login',
+    props<{ name: string, email: string }>(),
 );
 
 export const doLoginSuccess = createAction(
-  '[API] Do Login Success',
-  props<{ user: User }>(),
+    '[API] Do Login Success',
+    props<{ user: User }>(),
 );
 
 export const doLoginFailure = createAction(
-  '[API] Do Login Failure',
+    '[API] Do Login Failure',
 );
--- Cursos Angular/Aplicacoes Avancadas com Angular/jv-state-ngrx-master/src/app/state/app.reducer.ts
+++ Cursos Angular/Aplicacoes Avancadas com Angular/jv-state-ngrx-master/src/app/state/app.reducer.ts
@@ -3,21 +3,21 @@ import { User } from '../shared/models/user.model';
 import * as fromAppActions from './app.actions';
 
 export interface AppState {
-  user: User;
+    user: User;
 }
 
 export const initialState: AppState = {
-  user: undefined,
+    user: undefined
 };
 
 const appStateReducer = createReducer(
-  initialState,
-  on(fromAppActions.doLoginSuccess, (state, { user }) => ({
-    ...state,
-    user,
-  })),
+    initialState,
+    on(fromAppActions.doLoginSuccess, (state, { user }) => ({
+        ...state,
+        user,
+    })),
 );
 
 export function reducer(state: AppState | undefined, action: Action): AppState {
-  return appStateReducer(state, action);
+    return appStateReducer(state, action);
 }
