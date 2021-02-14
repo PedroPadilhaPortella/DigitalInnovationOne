08399c6a367f48477d07eb825f8bf26ee28f9ab9
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-02-13 23:59:24 -0300

    webpack

---
+++ Cursos React/Introdução ao ReactJS/webpack/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from './Button';
+import Card from './Card';
+
+
+const App = () => {
+  return (
+    <div>
+      <p>Digital Innovation One</p>
+      <p>Webpack com React</p>
+      <Button />
+      <Card />
+    </div>
+  );
+};
+
+export default App;
