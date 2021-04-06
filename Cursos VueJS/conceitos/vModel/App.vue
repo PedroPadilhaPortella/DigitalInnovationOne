8256be23a5856ffc9e050ccd2b15fc6c7928e570
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2021-04-06 00:20:29 -0300

    projeto vue pokedex

---
+++ Cursos VueJS/conceitos/vModel/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <input type="text" @input="text = $event.target.value" :value="text"  />
+    <input type="text" v-model.trim="text"  />
+    <input type="number" v-model.number="n"  />
+    {{ text }}    {{ n }}
+    <hr>
+    <input type="checkbox" v-model="check">
+    {{ check }}
+    <Input v-model="inputString" /> {{ inputString }}
+  </div>
+</template>
+
+<script>
+import Input from './Input';
+export default {
+    name: 'App',
+    components: { Input },
+    data() {
+        return {
+            text: '',
+            check: false,
+            n: 0,
+            inputString: ''
+        };
+    },
+    watch: {
+        text(value) {
+            console.log(value);
+        }
+    }
+}
+</script>
+
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+    #app {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+    input {
+        margin: 0.5rem;
+    }
+</style>
---
+++ Cursos VueJS/pokedex-dio/src/components/Pokedex.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="pokedex">
+        <TopPanel />
+        <LeftPanel  class="pokedex--left"/>
+        <Divider />
+        <LeftPanel  class="pokedex--right"/>
+    </div>
+</template>
+
+<script>
+import { TopPanel } from "./TopPanel/TopPanel.vue";
+import { LeftPanel } from "./LeftPanel/LeftPanel.vue";
+import { RightPanel } from "./RightPanel/RightPanel.vue";
+import { Divider } from "./Divider/Divider.vue";
+
+export default {
+    name: 'Pokedex',
+    components: { TopPanel, LeftPanel, RightPanel, Divider }
+}
+</script>
+
+<style lang="scss" scoped>
+    .pokedex {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        max-width: 960px;
+        height: 100%;
+
+        @media (min-width: $viewport-medium) {
+            max-height: 700px;
+            flex-direction: row;
+        }
+
+        &--left,
+        &--right {
+            position: relative;
+            display: flex;
+            height: 50%;
+            perspective: 1700px;
+            transform: preserve-3d;
+
+            @media (min-width: $viewport-medium) {
+                height: 100%;
+            }
+        }
+    }
+</style>
